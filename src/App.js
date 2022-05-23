cd7fd51fa2a4f3a7dc168a233601aa9e97acac10
Aditya23489e <99119876+Aditya23489e@users.noreply.github.com>
2022-05-23 17:36:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { AddTask } from "./components/AddTask";
+
+function App() {
+  return <div>{/* Code Here */
+  <AddTask></AddTask>
+  }</div>;
+}
+
+export default App;
---
+++ src/components/AddTask/AddTask.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./addTask.module.css";
+
+const AddTask = () => {
+  // NOTE: do not delete `data-cy` key value pair
+  return (
+    <div className={styles.todoForm}>
+      <div className={styles.heading}>
+      <h1>Todo List</h1>
+      </div>
+      <input data-cy="add-task-input" type="text" placeholder="Add Task..." />
+      <button data-cy="add-task-button">+</button>
+      
+    </div>
+  );
+};
+
+export default AddTask;
